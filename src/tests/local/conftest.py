0cbedcc9b3e33efdee3285c7509ec354b2c528df
Pamela Fox <pamela.fox@gmail.com>
2023-11-20 23:13:29 +0000

    Readme and conftest fix

--- src/tests/local/conftest.py
+++ src/tests/local/conftest.py
@@ -13,14 +13,18 @@ def app(request):
     config_override = {
         "TESTING": True,
         # Allows for override of database to separate test from dev environments
-        "SQLALCHEMY_DATABASE_URI": os.environ.get("TEST_DATABASE_URL", os.environ.get("DATABASE_URI")),
+        "SQLALCHEMY_DATABASE_URI": os.environ.get(
+            "TEST_DATABASE_URL", os.environ.get("DATABASE_URI")
+        ),
     }
     app = create_app(config_override)
 
     with app.app_context():
         engines = db.engines
         db.create_all()
-        seeder.seed_data(db, pathlib.Path(__file__).parent.parent.parent / "seed_data.json")
+        seeder.seed_data(
+            db, pathlib.Path(__file__).parent.parent.parent / "seed_data.json"
+        )
 
     engine_cleanup = []
 
