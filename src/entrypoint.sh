af9bb9983a6aebf156a95a009385eac545ea93a2
Pamela Fox <pamela.fox@gmail.com>
2023-11-20 18:54:09 +0000

    DOnt drop yall

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 python3 -m pip install --upgrade pip
 python3 -m pip install -e .
 python3 -m flask --app flaskapp db upgrade --directory flaskapp/migrations
-python3 -m flask --app flaskapp seed --filename="seed_data.json" --drop
+python3 -m flask --app flaskapp seed --filename="seed_data.json"
 python3 -m gunicorn "flaskapp:create_app()"
